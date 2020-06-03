fcb84b075e17a92fffb3e6ca80d78b84e02af75f
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-03 15:53:53 -0400

    Add project files.

---
+++ Chapter3Studio/CharacterCount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter3Studio
+{
+    public class CharacterCount
+    {
+        public static Dictionary<char, int> CharacterTypeCount(char[] array)
+        {
+            Dictionary<char, int> characterCount = new Dictionary<char, int>();
+            foreach (char letter in array)
+            {
+                if (characterCount.ContainsKey(letter))
+                    characterCount[letter]++;
+                else
+                    characterCount.Add(letter, 1);
+            }
+            foreach (KeyValuePair<char, int> character in characterCount)
+            {
+                Console.WriteLine(character.Key + ":" + character.Value);
+            }
+
+            return characterCount;
+        }
+    }
+}
---
+++ Chapter3Studio/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Chapter3Studio
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            char[] lipsum = loremIpsum.ToCharArray();
+            Console.WriteLine(lipsum.Length);
+            var loremIpsumCharCount = CharacterCount.CharacterTypeCount(lipsum);
+            Console.WriteLine(loremIpsumCharCount.Count);
+        }
+    }
+}
